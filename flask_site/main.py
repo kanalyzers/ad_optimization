892bf4494a70f219a257f99a8d020048cfac6ce6
ryan <ryanwkoch@gmail.com>
2019-04-26 09:52:15 -0700

    changed port to 8000 in main.py

--- flask_site/main.py
+++ flask_site/main.py
@@ -189,7 +189,7 @@ def server_error(e):
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
     #http_server = WSGIServer(('', 5000), app)
     #http_server.serve_forever()
     # [END gae_flex_quickstart]
